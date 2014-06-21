195f56f7560e93cb40f4f0ca0c73252e0f82a4c3
Michał Gołębiowski <m.goleb@gmail.com>
2014-06-21 02:23:30 +0200

    Add a test for install parameter

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var chalk = require('chalk'),
-    expect = require('chai').expect,
+    fs = require('fs-extra'),
+    semver = require('semver'),
+    chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect,
     checkDependencies = require('../check-dependencies');
 
 describe('checkDependencies', function () {
@@ -34,7 +38,7 @@ describe('checkDependencies', function () {
             scopeList: ['dependencies', 'devDependencies'],
             install: false,
         }, function (error) {
-            expect(error).to.equal(undefined);
+            expect(error).to.not.exist;
             expect(output.error).to.eql([]);
             done();
         });
@@ -46,7 +50,7 @@ describe('checkDependencies', function () {
             scopeList: ['dependencies', 'devDependencies'],
             install: false,
         }, function (error) {
-            expect(error).not.to.equal(undefined);
+            expect(error).to.exist;
             expect(output.error).to.eql([
                 'a: installed: 1.2.4, expected: 1.2.3',
                 'b: installed: 0.9.9, expected: >=1.0.0',
@@ -62,7 +66,7 @@ describe('checkDependencies', function () {
             scopeList: ['devDependencies'],
             install: false,
         }, function (error) {
-            expect(error).to.equal(undefined);
+            expect(error).to.not.exist;
             expect(output.error).to.eql([]);
             done();
         });
@@ -72,7 +76,7 @@ describe('checkDependencies', function () {
         checkDependencies({
             install: false,
         }, function (error) {
-            expect(error).to.equal(undefined);
+            expect(error).to.not.exist;
             expect(output.error).to.eql([]);
             done();
         });
@@ -90,9 +94,43 @@ describe('checkDependencies', function () {
 
     it('should allow to provide callback as the first argument', function (done) {
         checkDependencies(function (error) {
-            expect(error).to.equal(undefined);
+            expect(error).to.not.exist;
             expect(output.error).to.eql([]);
             done();
         })
     });
+
+    it('should install missing packages when install parameter is not set to false', function (done) {
+        var versionRange = require('./not-ok-install/package.json').dependencies['check-dependencies'],
+            version = JSON.parse(fs.readFileSync(__dirname +
+                '/not-ok-install/node_modules/check-dependencies/package.json')).version;
+
+        assert.equal(semver.satisfies(version, versionRange),
+            false, 'Expected version ' + version + ' not to match ' + versionRange);
+        this.timeout(30000);
+
+        fs.remove(__dirname + '/not-ok-install-copy', function (error) {
+            expect(error).to.not.exist;
+            fs.copy(__dirname + '/not-ok-install', __dirname + '/not-ok-install-copy',
+                function (error) {
+                    expect(error).to.not.exist;
+                    checkDependencies({
+                        packageDir: './test/not-ok-install-copy/',
+                    }, function (error) {
+                        expect(error).not.to.exist;
+                        expect(output.error).to.eql([
+                            'check-dependencies: installed: 0.4.1, expected: <=0.2.5',
+                        ]);
+                        version = JSON.parse(fs.readFileSync(__dirname +
+                            '/not-ok-install-copy/node_modules/' +
+                            'check-dependencies/package.json')).version;
+                        assert.equal(semver.satisfies(version, versionRange),
+                            true, 'Expected version ' + version +
+                                ' to match ' + versionRange);
+                        done();
+                    });
+                });
+        });
+    });
+
 });
